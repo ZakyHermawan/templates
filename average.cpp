7ffaf3b793b23ff4a87d91a0f433408c7f04c424
ZakyHermawan <zaky.hermawan9615@gmail.com>
2021-01-23 14:24:46 +0700

    Initial commit

---
+++ average.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Cents
+{
+private:
+    int m_cents;
+
+public:
+    Cents(int cents)
+        : m_cents{ cents }
+    {
+
+    }
+
+    friend bool operator> (const Cents& a, const Cents& b) {
+        return a.m_cents > b.m_cents;
+    }
+
+    friend std::ostream& operator<< (std::ostream& out, const Cents& cents) {
+        out << cents.m_cents << " cents" << endl;
+        return out;
+    }
+
+    Cents operator+= (const Cents& cents) {
+        m_cents += cents.m_cents;
+        return *this;
+    }
+
+    Cents operator/= (int value) {
+        m_cents /= value;
+        return *this;
+    }
+
+};
+
+template<class T>
+T average(T* array, int length) {
+    T sum(0);
+    for (int count{ 0 }; count < length; ++count) {
+        sum += array[count];
+    }
+
+    sum /= length;
+    return sum;
+}
+
+
+int main() {
+    Cents arr[]{ Cents(5), Cents(4), Cents(3) };
+    std::cout << average(arr, 3) << std::endl;
+
+    return 0;
+}
---
+++ static_array_container.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cassert>
+
+using std::cout;
+using std::endl;
+
+template<class T, int size>
+class StaticArray
+{
+private:
+    T m_arr[size];
+
+public:
+    StaticArray() = default;
+
+    T* getArray();
+
+    T& operator[] (int index) {
+        return m_arr[index];
+    }
+};
+
+template<class T, int size>
+T* StaticArray<T, size>::getArray() {
+    return m_arr;
+}
+
+int main() {
+    StaticArray<int, 12> IntArray;
+
+    for (int i{ 0 }; i < 12; ++i) {
+        IntArray[i] = i + 1;
+    }
+
+    for (int i = 11; i >= 0; --i) {
+        cout << IntArray[i] << ' ';
+    }
+    cout << endl;
+
+    StaticArray<double, 4> doubleArray;
+
+    for (int count = 0; count < 4; ++count)
+        doubleArray[count] = 4.4 + 0.1 * count;
+
+    for (int count = 0; count < 4; ++count)
+        cout << doubleArray[count] << ' ';
+    cout << endl;
+
+
+
+    return 0;
+}
